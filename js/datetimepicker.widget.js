722288b19b988611b8c08ccb415596662f8651b9
Florian Weber <florian.weber@ueberbit.de>
2014-09-23 10:58:11 +0200

    Use context from drupal behavior

--- js/datetimepicker.widget.js
+++ js/datetimepicker.widget.js
@@ -7,7 +7,7 @@
 
   Drupal.behaviors.datetimepicker = {
     attach: function (context, settings) {
-      $('[data-datetimepicker-widget]').once().each(function (i, v) {
+      $('[data-datetimepicker-widget]', context).once().each(function (i, v) {
         var element = $(this);
         element.attr('type', 'text');
 
